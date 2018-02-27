a563ce61e8daa3680d88d1e5b155284179795c7b
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2018-02-27 09:21:24 +0000

    Ruta raíz a new user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :tasks
   
-  root :to => 'tasks#index'
+  root :to => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
